15be5d733f49f8aebd366efd2aaacd188fa88963
Alex Smith <xelanybor@gmail.com>
2024-09-24 20:59:37 +0200

    implemented movement and jumping

--- Assets/Scripts/Marbles/MarbleController.cs
+++ Assets/Scripts/Marbles/MarbleController.cs
@@ -2,15 +2,66 @@ using UnityEngine;
 
 public class MarbleController : MonoBehaviour
 {
+    // Components
+    private Rigidbody2D rb;
+
+    // Constants
+    [Header("Basic Movement")]
+
+    [SerializeField] private float MAX_SPEED = 5f;
+    [SerializeField] private float ACCELERATION = 10f;
+    [SerializeField] private float JUMP_FORCE = 5f;
+
+    // State Variables
+
+    private bool canJump = true;
+
+    // Interior Values
+
+    private Vector2 movementInput;
+
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (movementInput.x > 0 && rb.linearVelocity.x < MAX_SPEED)
+        {
+            rb.AddForce(Vector2.right * ACCELERATION);
+        }
+        else if (movementInput.x < 0 && rb.linearVelocity.x > -MAX_SPEED)
+        {
+            rb.AddForce(Vector2.left * ACCELERATION);
+        }
     }
+
+    // Player Movement Methods
+
+    public void MovementInput(Vector2 input)
+    {
+        movementInput = input;
+    }
+
+    public void Jump()
+    {
+        rb.AddForce(Vector2.up * JUMP_FORCE, ForceMode2D.Impulse);
+        canJump = false;
+    }
+
+    // Collision Methods
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            canJump = true;
+        }
+    }
+
 }
--- Assets/Scripts/Marbles/PlayerController.cs
+++ Assets/Scripts/Marbles/PlayerController.cs
@@ -1,11 +1,16 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+
+    [SerializeField] private MarbleController marblePrefab;
+    private MarbleController marble;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        marble = Instantiate(marblePrefab);
     }
 
     // Update is called once per frame
@@ -13,4 +18,21 @@ public class PlayerController : MonoBehaviour
     {
         
     }
+
+    // Player Input Methods
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        marble.MovementInput(context.ReadValue<Vector2>());
+    }
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            marble.Jump();
+        }
+    }
+
+
 }
