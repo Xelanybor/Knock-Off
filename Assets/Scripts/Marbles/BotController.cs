68bc76e0b6fcb23a300d65515d8d9b63f6697357
Alex Smith <xelanybor@gmail.com>
2024-09-30 11:38:46 +0200

    created basic training bot

--- Assets/Scripts/Marbles/BotController.cs
+++ Assets/Scripts/Marbles/BotController.cs
@@ -2,15 +2,61 @@ using UnityEngine;
 
 public class BotController : MonoBehaviour
 {
+
+    [SerializeField] private MarbleController marblePrefab;
+    private MarbleController marble;
+    private Transform marbleTransform;
+
+    private float FLICK_COOLDOWN = 0.5f; // How long the bot waits between flicks
+    private float flickCooldownTimer = 0; // Timer for the flick cooldown
+
+    private float FLICK_CHARGE_TIME = 0.2f; // How long the bot charges the flick
+    private float flickChargeTimer = 0; // Timer for the flick charge
+
+    private bool isFlicking = false; // Whether the bot is currently flicking
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        marble = Instantiate(marblePrefab, new Vector3(4, -1, 0), Quaternion.identity);
+        if (!marble.TryGetComponent<Transform>(out marbleTransform))
+        {
+            Debug.LogError("Marble prefab does not have a Transform component!");
+        }
+
+        // marble.MovementInput(Vector2.right);
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        if (flickCooldownTimer > 0)
+        {
+            flickCooldownTimer -= Time.deltaTime;
+        }
+        
+        if (flickChargeTimer > 0)
+        {
+            flickChargeTimer -= Time.deltaTime;
+            if (flickChargeTimer <= 0)
+            {
+                marble.ReleaseFlick();
+                marble.MovementInput(Vector2.zero);
+                isFlicking = false;
+            }
+        }
+
+        if (Mathf.Abs(marbleTransform.position.x) > 8.5f && !isFlicking && flickCooldownTimer <= 0)
+        {
+            float direction = marbleTransform.position.x > 0 ? -1 : 1;
+            marble.MovementInput(new Vector2(direction, 0.4f));
+            flickChargeTimer = FLICK_CHARGE_TIME;
+            isFlicking = true;
+            flickCooldownTimer = FLICK_COOLDOWN;
+            marble.StartChargingFlick();
+
+        }
         
     }
 }
--- Assets/Scripts/Marbles/PlayerController.cs
+++ Assets/Scripts/Marbles/PlayerController.cs
@@ -10,7 +10,7 @@ public class PlayerController : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        marble = Instantiate(marblePrefab);
+        marble = Instantiate(marblePrefab, new Vector3(-4, -1, 0), Quaternion.identity);
     }
 
     // Update is called once per frame
