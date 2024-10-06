9a7029bbb0f0466cf370e9dfd76a8a4fea7dd3d5
dangor18 <dangordon.za@gmail.com>
2024-10-06 19:59:29 +0200

    fixed flick bar rotating with player

--- Assets/Scripts/Game_UI/flickBarUI.cs
+++ Assets/Scripts/Game_UI/flickBarUI.cs
@@ -7,14 +7,27 @@ public class flickBarUI : MonoBehaviour
     [SerializeField] private Image chargeBar;       // actually the border for charge increments
     [SerializeField] private Image incrementBar;    // for incrementing the charges over time
     [SerializeField] private Sprite[] chargeSprites;    // list of sprites for charging
+
+    [SerializeField] private Vector3 worldOffset = new Vector3(0, 0, 0);
+    private Transform playerTransform;
+    private Quaternion fixedRotation;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         player = GetComponentInParent<MarbleController>();  // get the player for this UI object
+        playerTransform = player.transform;
 
         incrementBar.fillAmount = 0.0f;
         player.OnProgressCharge += Player_OnProgressCharge;
         chargeBar.sprite = chargeSprites[0];
+        // Store the initial local position and rotation
+        fixedRotation = Quaternion.identity;
+    }
+
+    void LateUpdate()
+    {
+        transform.rotation = fixedRotation;
+        transform.position = playerTransform.position + worldOffset;
     }
 
     private void Player_OnProgressCharge(object sender, MarbleController.OnFlickBarCharge e)
