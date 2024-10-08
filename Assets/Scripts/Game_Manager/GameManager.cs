b045312679686078923cca2d161fbb053e1b23ff
Alex Smith <xelanybor@gmail.com>
2024-10-08 16:53:09 +0200

    fixed non-reachable code
    temporary fix that I forgot to remove before pushing oopsie

--- Assets/Scripts/Game_Manager/GameManager.cs
+++ Assets/Scripts/Game_Manager/GameManager.cs
@@ -150,7 +150,6 @@ public class GameManager : MonoBehaviour
 
     private void PaintLobbyUI()
     {
-        return;
         //First, destroy any existing UI elements(this is optional but helps reset the UI)
         foreach (Transform child in GameObject.FindWithTag("Canvas").transform)
         {
