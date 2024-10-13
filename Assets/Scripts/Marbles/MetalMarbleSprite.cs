5eae5de6ff282330d7a57a06a649aa67b2460656
Alex Smith <xelanybor@gmail.com>
2024-10-13 21:48:08 +0200

    added visual effect for metal marble

---
+++ Assets/Scripts/Marbles/MetalMarbleSprite.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class MetalMarbleSprite : MonoBehaviour
+{
+    
+    [SerializeField] Transform parentTransform; // Reference to the parent transform
+    [SerializeField] SpriteRenderer spriteRenderer; // Reference to the sprite renderer
+
+    private bool active = false; // Boolean to check if the marble is metal
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (active) transform.rotation = Quaternion.identity;
+    }
+
+    // Function to set the marble as metal
+    public void Enable()
+    {
+        Debug.Log("Metal Marble Enabled");
+        active = true;
+        spriteRenderer.enabled = true;
+    }
+    public void Disable()
+    {
+        spriteRenderer.enabled = false;
+        active = false;
+    }
+}
